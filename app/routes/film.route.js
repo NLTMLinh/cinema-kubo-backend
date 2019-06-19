5aa6ea704603a8fc8915722e43aa04cd0fe0a7b9
MyLinhIT <1612334@student.hcmus.edu.vn>
2019-06-19 12:13:06 +0700

    thêm đường dẫn trong route

--- app/routes/film.route.js
+++ app/routes/film.route.js
@@ -1,6 +1,9 @@
 module.exports = app => {
-    const film = require("../controllers/film.controller");
+  const film = require("../controllers/film.controller");
 
-    app.get("/film", film.read);
-    app.post("/film", film.create);
+  app.get("/film", film.read);
+  app.post("/film", film.create);
+  app.put("/film", film.update);
+  app.delete("/film", film.delete);
+  app.get("/film/get-film-by-type", film.getFilmsInType);
 };
